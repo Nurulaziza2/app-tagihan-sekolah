3c25c9456085ae6129223e1fe5fca099e408612b
Nurulaziza2 <nurulazizah20190279@gmail.com>
2022-04-02 15:50:14 +0700

    buat tabel user

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,8 +33,8 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-                        <li>
-                            <a class="nav-link" href="">User</a>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('user.index') }}">User</a>
                         </li>
                         
                     </ul>
---
+++ resources/views/user_index.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Data User</div>
+
+                <div class="card-body">
+                <table class="table table-striped table-bordered">
+                    <thead>
+                        <th>No</th>
+                        <th>Nama</th>
+                        <th>Email</th>
+                        <th>Tanggal Buat</th>
+                        <th>Aksi</th>
+                    </thead>
+                    <tbody>
+                        
+                        @foreach ($models as $item)
+                        <tr>
+                            <td>{{ $loop->iteration }}</td>
+                            <td>{{ $item->name }}</td>
+                            <td>{{ $item->email }}</td>
+                            <td>{{ $item->created_at->format('d/m/Y H:i') }}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
