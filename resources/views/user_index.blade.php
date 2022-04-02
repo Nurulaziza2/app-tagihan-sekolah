f8d80d7c9fdee943f795c3f02a534ad9e15b3f5c
suryaasaputra <suryaaasaputra.s@gmail.com>
2022-04-02 20:20:05 +0700

    surya yg ubah

--- resources/views/user_index.blade.php
+++ resources/views/user_index.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Data User</div>
+                <div class="card-header">Tabel Data User</div>
 
                 <div class="card-body">
                 <a href="{{ route('user.create') }}" class="btn btn-primary">Tambah Data</a>
