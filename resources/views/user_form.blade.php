f851f374eb44f7af9d7721c4a4320f14d139a787
Yyuud1 <yudaaanugrah1@gmail.com>
2022-04-04 13:58:44 +0700

    tes

--- resources/views/user_form.blade.php
+++ resources/views/user_form.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Tambah Data Userr</div>
+                <div class="card-header">Tambah Data User</div>
 
                 <div class="card-body">
                 {!! Form::model($model, ['route'=>$route, 'method'=>$method]) !!}
