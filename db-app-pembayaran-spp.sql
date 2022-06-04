2ba7867543dc5f2876402712b1af5a86b0aabc86
suryaasaputra <suryaaasaputra.s@gmail.com>
2022-06-04 17:03:07 +0700

    update db

--- db-app-pembayaran-spp.sql
+++ db-app-pembayaran-spp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 01, 2022 at 11:12 AM
+-- Generation Time: Jun 04, 2022 at 12:02 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 7.4.28
 
@@ -132,6 +132,7 @@ CREATE TABLE `password_resets` (
 CREATE TABLE `siswa` (
   `id` int(11) NOT NULL,
   `nama` varchar(255) NOT NULL,
+  `gambar` varchar(255) DEFAULT NULL,
   `nis` int(20) NOT NULL,
   `email` varchar(255) NOT NULL,
   `jk` varchar(20) NOT NULL,
@@ -145,22 +146,47 @@ CREATE TABLE `siswa` (
 -- Dumping data for table `siswa`
 --
 
-INSERT INTO `siswa` (`id`, `nama`, `nis`, `email`, `jk`, `tgl_masuk`, `user_id`, `created_at`, `updated_at`) VALUES
-(1, 'Aldi Taher', 8010, 'alditaher@gmail.com', 'Pria', '2022-05-29', 1, '2022-06-01 08:59:40', '2022-06-01 08:59:40'),
-(2, 'Ananda Ali Taher', 8011, 'anandaalitaher@gmail.com', 'Pria', '2022-05-29', 1, '2022-06-01 08:59:40', '2022-06-01 08:59:40'),
-(3, 'Ardan Badarudin', 8012, 'ardanbadarudin@gmail.com', 'Pria', '2022-05-30', 1, '2022-06-01 08:59:41', '2022-06-01 08:59:41'),
-(4, 'Arial Hunaiku', 8013, 'arialhunaiku@gmail.com', 'Pria', '2022-05-30', 1, '2022-06-01 08:59:41', '2022-06-01 08:59:41'),
-(5, 'Cristina Marsionova', 8014, 'cristinamarsionova@gmail.com', 'Wanita', '2022-05-30', 1, '2022-06-01 08:59:41', '2022-06-01 08:59:41'),
-(6, 'Gustixa', 8015, 'gustixa@gmail.com', 'Pria', '2022-05-30', 1, '2022-06-01 08:59:41', '2022-06-01 08:59:41'),
-(7, 'Irsandi', 8016, 'irsandi@gmail.com', 'Wanita', '2022-05-30', 1, '2022-06-01 08:59:41', '2022-06-01 08:59:41'),
-(8, 'Ismail', 8017, 'ismail@gmail.com', 'Pria', '2022-05-31', 1, '2022-06-01 08:59:41', '2022-06-01 08:59:41'),
-(9, 'Junaidi', 8018, 'junaidi@gmail.com', 'Pria', '2022-05-31', 1, '2022-06-01 08:59:41', '2022-06-01 08:59:41'),
-(10, 'Lord Rangga Sasana', 8019, 'lordranggasasana@gmail.com', 'Pria', '2022-05-31', 1, '2022-06-01 08:59:41', '2022-06-01 08:59:41'),
-(11, 'Muh Irsank', 8020, 'muhirsank@gmail.com', 'Pria', '2022-05-31', 1, '2022-06-01 08:59:41', '2022-06-01 08:59:41'),
-(12, 'Muhammad Rizkhal Lamaau', 8021, 'muhammadrizkhallamaau@gmail.com', 'Pria', '2022-05-01', 1, '2022-06-01 08:59:41', '2022-06-01 08:59:41'),
-(13, 'Rony Marteen', 8022, 'ronymarteen@gmail.com', 'Pria', '2022-05-01', 1, '2022-06-01 08:59:41', '2022-06-01 08:59:41'),
-(14, 'Slamet', 8023, 'slamet@gmail.com', 'Wanita', '2022-05-01', 1, '2022-06-01 08:59:41', '2022-06-01 08:59:41'),
-(15, 'Tretan Bukan Muslim', 8024, 'tretanbukanmuslim@gmail.com', 'Pria', '2022-05-01', 1, '2022-06-01 08:59:41', '2022-06-01 08:59:41');
+INSERT INTO `siswa` (`id`, `nama`, `gambar`, `nis`, `email`, `jk`, `tgl_masuk`, `user_id`, `created_at`, `updated_at`) VALUES
+(1, 'Aldi Taher', '', 8010, 'alditaher@gmail.com', 'Pria', '2022-05-29', 1, '2022-06-02 02:15:10', '2022-06-02 02:15:10'),
+(2, 'Ananda Ali Taher', '', 8011, 'anandaalitaher@gmail.com', 'Pria', '2022-05-29', 1, '2022-06-02 02:15:10', '2022-06-02 02:15:10'),
+(3, 'Ardan Badarudin', '', 8012, 'ardanbadarudin@gmail.com', 'Pria', '2022-05-29', 1, '2022-06-02 02:15:10', '2022-06-02 02:15:10'),
+(4, 'Arial Hunaiku', '', 8013, 'arialhunaiku@gmail.com', 'Pria', '2022-05-29', 1, '2022-06-02 02:15:10', '2022-06-02 02:15:10'),
+(5, 'Cristina Marsionova', '', 8014, 'cristinamarsionova@gmail.com', 'Wanita', '2022-05-29', 1, '2022-06-02 02:15:10', '2022-06-02 02:15:10'),
+(6, 'Gustixa', '', 8015, 'gustixa@gmail.com', 'Pria', '2022-05-29', 1, '2022-06-02 02:15:10', '2022-06-02 02:15:10'),
+(7, 'Irsandi', '', 8016, 'irsandi@gmail.com', 'Wanita', '2022-05-29', 1, '2022-06-02 02:15:10', '2022-06-02 02:15:10'),
+(8, 'Ismail', '', 8017, 'ismail@gmail.com', 'Pria', '2022-05-30', 1, '2022-06-02 02:15:10', '2022-06-02 02:15:10'),
+(9, 'Junaidi', '', 8018, 'junaidi@gmail.com', 'Pria', '2022-05-30', 1, '2022-06-02 02:15:10', '2022-06-02 02:15:10'),
+(10, 'Lord Rangga Sasana', '', 8019, 'lordranggasasana@gmail.com', 'Pria', '2022-05-30', 1, '2022-06-02 02:15:10', '2022-06-02 02:15:10'),
+(11, 'Muh Irsank', '', 8020, 'muhirsank@gmail.com', 'Pria', '2022-05-30', 1, '2022-06-02 02:15:10', '2022-06-02 02:15:10'),
+(12, 'Muhammad Rizkhal Lamaau', '', 8021, 'muhammadrizkhallamaau@gmail.com', 'Pria', '2022-05-30', 1, '2022-06-02 02:15:10', '2022-06-02 02:15:10'),
+(13, 'Rony Marteen', '', 8022, 'ronymarteen@gmail.com', 'Pria', '2022-05-31', 1, '2022-06-02 02:15:10', '2022-06-02 02:15:10'),
+(14, 'Slamet', '', 8023, 'slamet@gmail.com', 'Wanita', '2022-05-31', 1, '2022-06-02 02:15:10', '2022-06-02 02:15:10'),
+(15, 'Tretan Bukan Muslim', '', 8024, 'tretanbukanmuslim@gmail.com', 'Pria', '2022-05-31', 1, '2022-06-02 02:15:10', '2022-06-02 02:15:10'),
+(16, 'Tretan Muslim', '', 8025, 'tretanmuslim1@gmail.com', 'Laki-laki', '2022-05-31', 1, '2022-06-02 02:15:10', '2022-06-03 02:22:54'),
+(17, 'tretan', 'public/images/agYrazlrBRahbMM6NptJxAkovr2plmUd17q8e4jC.jpg', 7009, 'treaten@gmail.com', 'Laki-laki', '2022-06-01', 1, '2022-06-03 02:24:15', '2022-06-03 02:24:15');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tagihan`
+--
+
+CREATE TABLE `tagihan` (
+  `id` int(11) NOT NULL,
+  `siswa_id` int(11) NOT NULL,
+  `tanggal_tagihan` date NOT NULL,
+  `tanggal_jatuh_tempo` date NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `jumlah` double NOT NULL,
+  `keterangan` varchar(255) DEFAULT NULL,
+  `denda` int(11) NOT NULL DEFAULT 0,
+  `status` varchar(20) NOT NULL,
+  `tanggal_bayar` date NOT NULL,
+  `dibayar_oleh` varchar(255) NOT NULL,
+  `dibuat_oleh` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -184,8 +210,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `akses`, `created_at`, `updated_at`) VALUES
-(1, 'admin', 'admin@mail.com', NULL, '$2y$10$GUOdLZZj3c4KF6p5j4PLiOpbHvaEJ7Te2DU.1F3hkOBOXOKZlevGm', 'admin', '2022-05-25 08:03:41', '2022-05-25 08:03:41'),
-(4, 'surya', 'surya@mail.com', NULL, '$2a$12$CNMhP2acLAtAevgHMPfARuizR14spzFE1Jn2/qrBL05exj4W04T8a', 'operator', '2022-05-30 08:44:43', '2022-05-30 08:44:43');
+(1, 'Ketua', 'admin@mail.com', NULL, '$2y$10$GUOdLZZj3c4KF6p5j4PLiOpbHvaEJ7Te2DU.1F3hkOBOXOKZlevGm', 'admin', '2022-05-25 08:03:41', '2022-06-02 02:43:47'),
+(4, 'Surya Maulana Saputra', 'surya@mail.com', NULL, '$2a$12$CNMhP2acLAtAevgHMPfARuizR14spzFE1Jn2/qrBL05exj4W04T8a', 'operator', '2022-05-30 08:44:43', '2022-06-02 02:47:30');
 
 --
 -- Indexes for dumped tables
@@ -228,6 +254,12 @@ ALTER TABLE `siswa`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `tagihan`
+--
+ALTER TABLE `tagihan`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -265,7 +297,13 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `siswa`
 --
 ALTER TABLE `siswa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `tagihan`
+--
+ALTER TABLE `tagihan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
