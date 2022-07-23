e891cd23c431c020fcab7b565015d4ce611545cb
Yyuud1 <yudaaanugrah1@gmail.com>
2022-07-23 12:32:35 +0700

    update kwitansi & kartu spp ver.2

--- resources/views/operator/kartu_spp.blade.php
+++ resources/views/operator/kartu_spp.blade.php
@@ -12,59 +12,107 @@
         @import url('https://fonts.googleapis.com/css2?family=Quicksand&display=swap');
         body {
             font-family: 'Quicksand', sans-serif;
-
         }
-      
+        header {
+            /* background-color: red; */
+            width: 100%;
+            /* padding: 10px; */
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            text-align: center;
+            border-bottom: 1px solid #777;
+            position: relative;
+        }
+        header .logo img {
+            width: 100px;
+            border-radius: 50%;
+            position: absolute;
+            top: 2px;
+            left: 2%;
+        }
+        header ul li {
+            display: block;
+            list-style: none;
+            margin: 10px 0;
+        }
+        header ul li h2 {
+            font-weight: bold;
+        }
+        .container .card-title {
+            margin-left: 10%;
+            font-weight: bold;
+        }
+        .container .table2 thead tr {
+            background-color: rgba(0, 0, 0, 0.1);
+        }
+        .container blockquote {
+            padding: 20px;
+            background-color: rgba(0, 0, 0, 0.08);
+            font-weight: bold;
+        }
     </style>
 </head>
 <body>
-    <div class="container mt-3">
+    <header>
+        <div class="logo">
+            <img src="{{ asset('stisla') }}/assets/img/logo.jpeg" alt="">
+        </div>
+
+        <ul>
+            <li><h2>FA SEKOLAH MODE</h2></li>
+            <li><p>Jln. Halim Perdana Kusuma No.02 Jambi, Indonesia 36123</p></li>
+        </ul>
+    </header>
+
+    <div class="container mt-4">
         <div class="row">
             <div class="col-sm-12">
                 <div class="card">
                     <div class="card-body">
                         <div class="row">
                             <div class="col-sm-11">
-                                <h4 class="card-title">KARTU SPP TAHUN {{ $model->tanggal_tagihan->translatedFormat('Y') }}</h4>
+                                <h4 class="card-title text-center">KARTU PEMBAYARAN SPP BULANAN </h4>
                             </div>
                         </div>
     
-                        <table class="tabel ">
-                            <tr>
+                        <table class="tabel mt-3">
+                            {{-- <tr>
                                 <td rowspan="6" width="250">
                                     <img src="{{ \Storage::url($model->siswa->gambar ?? 'images/no-image.png') }}"
                                         alt="gbr" width="250" class="mt-3 rounded">
                                 </td>
-                            </tr>
+                            </tr> --}}
                             <tr>
-                                <td width="20%">NIS</td>
-                                <td>{{ $model->siswa->nis }}</td>
+                                <td width="200px" >NIS</td>
+                                <td>: {{ $model->siswa->nis }}</td>
                             </tr>
                             <tr>
                                 <td>Nama</td>
-                                <td>{{ $model->siswa->nama }}</td>
+                                <td>: {{ $model->siswa->nama }}</td>
                             </tr>
                             <tr>
                                 <td>Email</td>
-                                <td>{{ $model->siswa->email }}</td>
+                                <td>: {{ $model->siswa->email }}</td>
                             </tr>
                             <tr>
                                 <td>Program Kursus</td>
-                                <td>{{ $model->siswa->kelas->nama }} {{ $model->siswa->durasi }}</td>
+                                <td>: {{ $model->siswa->kelas->nama }} {{ $model->siswa->durasi }}</td>
                             </tr>
                             <tr>
                                 <td>Tanggal Masuk</td>
-                                <td>{{ $model->siswa->tgl_masuk->translatedFormat('d F Y') }}</td>
+                                <td>: {{ $model->siswa->tgl_masuk->translatedFormat('d F Y') }}</td>
                             </tr>
-                        </table>
+                        </table><br>
 
-                        <table class="table">
+                        <table class="table table2">
                             <thead>
                                 <tr>
                                     <th>NO</th>
                                     <th>Periode</th>
                                     <th>Jumlah</th>
                                     <th>Status</th>
+                                    <th>Paraf</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -83,12 +131,28 @@
     
                                     <td>
                                         <a href={{ route('tagihan.show', $item->id) }} ><div class="badge {{ $item->status ==='Lunas' ? 'badge-success' : 'badge-danger' }}">{{ $item->status }}</div></a>
-                                    </td>    
+                                    </td> 
+                                    
+                                    <td>
+
+                                    </td>
                                 </tr>
                                 @endforeach
     
                             </tbody>
                         </table>
+                        <div class="row mt-5">
+                            <div class="col-md-9">
+                                <blockquote>
+                                    *Pembayaran SPP paling lambat setiap tanggal 10 pada bulan jatuh tempo, jika terlambat maka akan dikenakan denda Rp. 2000/hari.
+                                </blockquote>
+                            </div>
+                            <div class="col-md-3">
+                                Jambi, {{date('d F Y')}}
+                                <br><br><br>
+                                <u>{{Auth::user()->name}}</u>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
--- resources/views/operator/kwitansi.blade.php
+++ resources/views/operator/kwitansi.blade.php
@@ -12,37 +12,89 @@
         @import url('https://fonts.googleapis.com/css2?family=Quicksand&display=swap');
         body {
             font-family: 'Quicksand', sans-serif;
-
+        }
+        header {
+            /* background-color: red; */
+            width: 100%;
+            /* padding: 10px; */
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            text-align: center;
+            border-bottom: 1px solid #777;
+            position: relative;
+        }
+        header .logo img {
+            width: 100px;
+            border-radius: 50%;
+            position: absolute;
+            top: 2px;
+            left: 2%;
+        }
+        header ul li {
+            display: block;
+            list-style: none;
+            margin: 10px 0;
+        }
+        header ul li h2 {
+            font-weight: bold;
         }
         .container h4 {
             position: relative;
-            background-color: #168aad;
             font-weight: bold;
-            color: #fff;
             text-transform: uppercase;
             border-radius: 5px;
             font-family: 'Quicksand', sans-serif;
         }
-        .container h4 .logo {
-            position: absolute;
-            top: 3px;
-            left: 5px;
-            border-radius: 5px;
-            width: 53px;
-        }
         .container .row table {
-            background-color: rgba(0, 0, 0, 0.09);
+            background-color: rgba(0, 0, 0, 0.03);
             border-radius: 5px;
+        }
+        .container .nomor p{
+            border-bottom: 1px solid rgba(0, 0, 0, 0.4);
+            width: 10rem;
+        }
+        .container .tgl p{
+            border-bottom: 1px solid rgba(0, 0, 0, 0.4);
+            width:16rem;
+        }
+        .container .row .terbilang {
+            padding: 20px;
+        }
+        .container .row .terbilang p {
+            padding: 35px;
+            background-color: rgba(0, 0, 0, 0.08);
+            width: 20rem;
         }
     </style>
 </head>
 <body>
+    <header>
+        <div class="logo">
+            <img src="{{ asset('stisla') }}/assets/img/logo.jpeg" alt="">
+        </div>
+
+        <ul>
+            <li><h2>FA SEKOLAH MODE</h2></li>
+            <li><p>Jln. Halim Perdana Kusuma No.02 Jambi, Indonesia 36123</p></li>
+        </ul>
+    </header>
+
     <div class="container">
-        <h4 class="text-center p-3 mt-5">
-            <img src="{{ asset('stisla') }}/assets/img/logo.jpeg" alt="logo" class="logo">
+        <h4 class="text-center p-3 mt-3">
             Kwitansi Pembayaran SPP Bulan {{ $model->tanggal_tagihan->translatedFormat('F Y') }}
         </h4>
-        <div class="row">
+
+        <div class="row ">
+            <div class="nomor col-md-9">
+                <p>No.</p>
+            </div>
+            <div class="tgl col-md-3">
+                <p>Tanggal.</p>
+            </div>
+        </div>
+
+        <div class="row mt-3">
             <div class="col-md-12">
                 <table class="table table-hover">
                     <tr>
@@ -54,7 +106,7 @@
                     <td>: Rp.{{ $model->getJumlahRupiah() }}</td>
                     </tr>
                     <tr>
-                        <td>Program Kursus </td>
+                        <td>Untuk Pembayaran </td>
                         <td>: {{ $model->siswa->kelas->nama }} {{ $model->siswa->durasi }}</td>
                     </tr>
                     <tr >
@@ -63,8 +115,9 @@
                     </tr>
                 </table>
                 <div class="row">
-                    <div class="col-md-9">
-                        <u>Terbilang : {{$model->getJumlahTerbilang()}}</u> 
+                    <div class="terbilang col-md-9">
+                        {{-- {{$model->getJumlahTerbilang()}} --}}
+                        <p>RP. </p> 
                     </div>
                     <div class="col-md-3">
                         Jambi, {{date('d F Y')}}
