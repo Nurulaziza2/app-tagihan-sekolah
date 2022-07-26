9aee54da9faf1f5ee411a278dda573579c6cbc0e
suryaasaputra <suryaaasaputra.s@gmail.com>
2022-07-26 15:21:42 +0700

    update tampilan dashboard v.2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,8 @@
             </div>
         </div>
     </div>
-    <div class="row ">
+
+    {{--  <div class="row ">
         <div class="col-md-3">
             <div class="card ">       
                 <div class="card-body">
@@ -48,7 +49,69 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div>  --}}
+    <div class="row">
+            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+              <div class="card card-statistic-1">
+                <div class="card-icon bg-primary">
+                  <i class="far fa-user"></i>
+                </div>
+                <div class="card-wrap">
+                  <div class="card-header">
+                    <h4>Jumlah Siswa</h4>
+                  </div>
+                  <div class="card-body">
+                    {{ $jumlah_siswa }}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+              <div class="card card-statistic-1">
+                <div class="card-icon bg-danger">
+                  <i class="far fa-credit-card"></i>
+                </div>
+                <div class="card-wrap">
+                  <div class="card-header">
+                    <h4>Tagihan Belum Bayar</h4>
+                  </div>
+                  <div class="card-body">
+                    {{ $jumlah_tagihan }}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+              <div class="card card-statistic-1">
+                <div class="card-icon bg-success">
+                  <i class="far fa-credit-card"></i>
+                </div>
+                <div class="card-wrap">
+                  <div class="card-header">
+                    <h4>Tagihan Lunas</h4>
+                  </div>
+                  <div class="card-body">
+                    {{ $jumlah_tagihan_lunas }}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+              <div class="card card-statistic-1">
+                <div class="card-icon bg-info">
+                  <i class="fas fa-wallet"></i>
+                </div>
+                <div class="card-wrap">
+                  <div class="card-header">
+                    <h4>Total Pembayaran</h4>
+                  </div>
+                  <div class="card-body">
+                    Rp{{ number_format($jumlah_pembayaran,0,",",".") }} 
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
 
 
 
